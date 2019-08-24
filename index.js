4e579e49cdaf56293ca4d0c99e9ce849cf80df93
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2019-08-24 21:49:06 +0900

    First version

---
+++ index.js
@@ -0,0 +1,22 @@
+const { spawnSync } = require('child_process')
+
+const asyncSpawn = require('child-process-promise').spawn
+
+const spawnConfig = { shell: true, stdio: 'inherit' }
+
+const run = cmd => {
+  console.log(`\x1b[35mRunning\x1b[0m: ${cmd}`)
+  const result = spawnSync(cmd, spawnConfig)
+  if (result.status !== 0) {
+    process.exit(1)
+  }
+}
+
+const runAsync = cmd => {
+  console.log(`\x1b[35mRunning\x1b[0m (async): ${cmd}`)
+  return asyncSpawn(cmd, spawnConfig)
+}
+
+const scripts = scriptsObj => scriptsObj[process.argv[2]]()
+
+module.exports = { run, runAsync, scripts }
