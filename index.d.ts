7a0866305fd8e93aea65bfd9f6daabffaa17e1a8
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2019-09-04 16:05:03 +0900

    Fix TS definitions

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,10 @@
-declare function runSync(commandOrOptions: string | Object, options?: Object): void
-declare function runAsync(commandOrOptions: string | Object, options?: Object): Promise<any>
-declare function scripts(scriptsObj: Object): void
-declare function series(...commands: (string | Object)[]): void
-declare function parallel(...commands: (string | Object)[]): Promise<any[]>
+export function httpServer(options?: object): string
+export function serverlessDeploy(port?: string): string
+export function serverlessOffline(options?: object): string
+export function waitDockerPgReady(network: string): string
+export function webpackDevServer(port?: string): string
 
-export { runSync, runAsync, scripts, series, parallel }
+export const DOCKER_COMPOSE_UP: string
+export const WEBPACK_PROD: string
+export const SHX_COPY_PUBLIC_TO_DIST: string
+export const SHX_RM_DIST_DOTWEBPACK: string
